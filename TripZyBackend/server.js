bab962ebad9339ffd9f97ad470546e1dea677451
NuwaniSudarshika <nuwanisudarshi@gmail.com>
2026-03-01 10:03:34 +0530

    MogoDB connected

--- TripZyBackend/server.js
+++ TripZyBackend/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const roomRoutes = require('./routes/roomRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const reviewRoutes = require('./routes/reviewRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+// Import Routes
+const userRoutes = require('./routes/userRoutes');
+const roomRoutes = require('./routes/roomRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -21,4 +26,7 @@ app.use('/api/reviews', reviewRoutes);
 app.use('/api/payments', paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
